7c38c105a1831d88a295039f86f607284d826bf4
sarina-shakya <sarinashakya33@gmail.com>
2023-07-27 20:43:53 +0545

    Ch_10

---
+++ Ch_10/NEG_ex_hg.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+class Number{
+    private:
+        double num;
+    public:
+        class NEG{};
+        void readnum();
+        double sqroot();
+};
+void Number::readnum(){
+    cout<<"Enter number:";
+    cin>>num;
+}
+double Number::sqroot(){
+    if (num<0)
+        throw NEG();
+    else
+        return (sqrt(num));
+
+}
+int main(){
+    Number n1;
+    double res;
+    n1.readnum();
+    try{
+        cout<<"\nTrying to find squareroot...";
+        res=n1.sqroot();
+        cout<<"\nSquare root is:"<<res<<endl;
+        cout<<"Success... Exception is not raised"<<endl;
+    }
+    catch(Number::NEG){
+        cout<<"\nSquare root of negative number " "not possible!"<<endl;
+    }
+
+return 0;
+
+}
